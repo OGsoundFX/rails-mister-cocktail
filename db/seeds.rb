cf70492aa45df991208cd741d1cc46bb00978764
Olivier Girardot <olive_girardot@hotmail.com>
2020-02-21 16:42:55 +0100

    horuku

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,15 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
-#   Character.create(name: 'Luke', movie: movies.first)
+puts "cleaning DB"
+Ingredient.destroy_all
+puts "creating ingredients"
+Ingredient.create(name: "lemon")
+Ingredient.create(name: "ice")
+Ingredient.create(name: "mint leaves")
+Ingredient.create(name: "ginger")
+Ingredient.create(name: "tomato")
+Ingredient.create(name: "gin")
+Ingredient.create(name: "vodka")
+
+Cocktail.create(name: "Manhattan")
+Cocktail.create(name: "Gin Tonic")
+
+puts "Finished creating ingredients"
