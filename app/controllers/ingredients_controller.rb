f543724b29d88387f28fd38c419bc100d54d7a92
Olivier Girardot <olive_girardot@hotmail.com>
2020-05-23 15:27:39 +0200

    added the new and create actions in the ingredients controller

--- app/controllers/ingredients_controller.rb
+++ app/controllers/ingredients_controller.rb
@@ -1,2 +1,22 @@
 class IngredientsController < ApplicationController
+  def new
+    raise
+    @ingredient = Ingredient.new
+  end
+
+  def create
+    @cocktail = Cocktail.find(params[:id])
+    @ingredient = Ingredient.new(ingredient_params)
+    if @ingredient.save
+      redirect_to new_cocktail_dose_path(@cocktail)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def ingredient_params
+    params.require(:ingredient).permit(:name, :category)
+  end
 end
--- app/models/ingredient.rb
+++ app/models/ingredient.rb
@@ -1,5 +1,6 @@
 class Ingredient < ApplicationRecord
   has_many :doses
+  has_many :cocktails, through: :doses
   validates :name, presence: { message: 'should not be empty' }, uniqueness: true
   validates :category, presence: true, inclusion: { in: ["Alcohol", "Soda", "Juice", "Veggies", "Spice", "Other"] }
 end
