48b8c1cb55089f58746f440b8a40207b742f0105
Olivier Girardot <olive_girardot@hotmail.com>
2020-08-18 14:37:02 +0200

    added map to cocktail show page let's move on

--- app/controllers/cocktail_bars_controller.rb
+++ app/controllers/cocktail_bars_controller.rb
@@ -5,8 +5,19 @@ class CocktailBarsController < ApplicationController
     @markers = @bars.map do |bar|
       {
         lat: bar.latitude,
-        lng: bar.longitude
+        lng: bar.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { bar: bar })
       }
     end
+  end
+
+  def show
+    @bar = CocktailBar.geocoded.find(params[:id])
+    @markers =
+      [{
+        lat: @bar.latitude,
+        lng: @bar.longitude,
+        infoWindow: render_to_string(partial: "info_window", locals: { bar: @bar })
+      }]
   end
 end
